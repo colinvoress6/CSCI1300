c27065a5cde2abab5b2188925ea4f0727957d9e6
Colin Voress <covo6487@colorado.edu>
2025-10-13 13:10:36 -0600

    input stream

---
+++ FileStreams/instream.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <fstream>   // for file input/output
+#include <string>
+using namespace std;
+
+int main() {
+    // Challenge: Choose and learn one of the C++ keywords or concepts you're unfamiliar with
+
+    // TODO: Create an input file stream
+    ifstream inFile;
+
+    // TODO: Open the file "words.txt"
+    inFile.open("words.txt");
+
+    // TODO: Check if the file opened successfully
+    if (!inFile.is_open()) {
+        cout << "Error opening file!" << endl;
+        return 1;
+    }
+
+    string word;
+
+    // TODO: Read and display each word in the file
+    while (getline(inFile, word)) {
+        cout << word << endl;
+    }
+
+    // TODO: Close the file
+    inFile.close();
+    return 0;
+}
